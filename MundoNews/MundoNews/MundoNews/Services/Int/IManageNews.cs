22340bf4fe3631465b8fe2e55903fa5e311457dd
Joseph <josephville_12@hotmail.es>
2021-07-27 17:25:04 +0200

    MundoNewsInicial

---
+++ MundoNews/MundoNews/MundoNews/Services/Int/IManageNews.cs
@@ -0,0 +1,13 @@
+﻿using MundoNews.Models.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MundoNews.Services.Int
+{
+    public interface IManageNews
+    {
+        List<NewsObject> GetLatestNews();
+    }
+}
